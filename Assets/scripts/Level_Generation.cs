71af6f76db064128f877da16384e1bc1b3034827
dipstick <99173882+fuckallofya@users.noreply.github.com>
2024-08-28 11:59:01 +0100

    updated generation script

--- Assets/scripts/Level_Generation.cs
+++ Assets/scripts/Level_Generation.cs
@@ -10,18 +10,27 @@ public class Level_Generation : MonoBehaviour
     private float SpawnTime = 1;
     float incr = 0;
     float height = 0;
+
     public GameObject platform;
+    public GameObject platform2;
+    public List <GameObject> Platforms = new List<GameObject> ();
+
     public void Start()
     {
+        //Platforms.Add (platform);
+        //Platforms.Add(platform2);
         StartCoroutine(Spawn());
 
     }
 
     IEnumerator Spawn()
     {
-        Instantiate(platform, new Vector2(incr, height), Quaternion.identity);
+        int rndplatform = Random.Range(0, Platforms.Count);
+        Instantiate(Platforms[rndplatform], new Vector2(incr, height), Quaternion.identity);
+
         incr += Random.Range(2f, 5f);
         height = Random.Range(1.5f, -1.5f);
+
         yield return new WaitForSeconds(SpawnTime);
         StartCoroutine(Spawn());
     }
